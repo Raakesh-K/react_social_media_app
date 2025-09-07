09a94f822dee58bc7e092b1fd4f5f9af75328e91
Raakesh <146430871+Raakesh-K@users.noreply.github.com>
2025-09-07 12:44:04 +0530

    Fix: added date-fns dependency

--- src/assets/components/Missing.jsx
+++ src/assets/components/Missing.jsx
@@ -6,10 +6,7 @@ const Missing = () => {
   <h1>
     404 - Page Not Found
   </h1>
-  <p className="">Sorry, the page you are looking for does not exist.</p>
-  <Link to="/">
-    Go Home
-  </Link>
+  
 </main>
 
   )
